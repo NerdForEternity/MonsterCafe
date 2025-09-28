ef329f96c44e9b34e17bccdff66dc170aef781e9
Jayden Currence <64615434+NerdForEternity@users.noreply.github.com>
2025-09-28 16:30:59 -0400

    Finished Screen UI

---
+++ Assets/Scripts/ScreenMenuEvents1.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UIElements;
+using System.Runtime.CompilerServices;
+
+public class ScreenMenuEvents1 : MonoBehaviour
+{
+    private UIDocument _document;
+
+    private Button _GrimmButton;
+    private Button _PlayButton;
+    private Button _DecorButton;
+    int toggle = 0;
+
+    private void Awake()
+    {
+        _document = GetComponent<UIDocument>();
+
+        _GrimmButton = _document.rootVisualElement.Q("Grimm") as Button;
+        _PlayButton = _document.rootVisualElement.Q("Play") as Button;
+        _DecorButton = _document.rootVisualElement.Q("Decor") as Button;
+
+        _GrimmButton.RegisterCallback<ClickEvent>(OnGrimmClick);
+        _PlayButton.RegisterCallback<ClickEvent>(OnPlayClick);
+        _DecorButton.RegisterCallback<ClickEvent>(OnDecorClick);
+    }
+
+    private void OnDisable()
+    {
+        _GrimmButton.UnregisterCallback<ClickEvent>(OnGrimmClick);
+        _PlayButton.UnregisterCallback<ClickEvent>(OnPlayClick);
+        _DecorButton.UnregisterCallback<ClickEvent>(OnDecorClick);
+    }
+    private void OnGrimmClick(ClickEvent evt)
+    {
+        Debug.Log("Grimm");
+
+    }
+    private void OnPlayClick(ClickEvent evt)
+    {
+
+        Debug.Log("Play");
+        if (toggle == 0)
+        {
+            _PlayButton.style.backgroundImage = new StyleBackground(AssetDatabase.LoadAssetAtPath<Sprite>("Assets/Sprites/UI/Idle_Play.png"));
+            toggle = 1;
+        }
+        else if (toggle == 1)
+        {
+            _PlayButton.style.backgroundImage = new StyleBackground(AssetDatabase.LoadAssetAtPath<Sprite>("Assets/Sprites/UI/Active_Play.png"));
+            toggle = 0;
+        }
+
+    }
+    private void OnDecorClick(ClickEvent evt)
+    {
+        Debug.Log("Decor");
+    }
+}
